52da4256363703d3d082a2972196403c1b18be12
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-06-20 01:13:42 +0300

    Minor stuff

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var buttons = document.getElementsByClassName( 'btn' );
 var numResult, isOperator, currentOperator, currentButton, firstNum, secondNum;
 var buttonStyle = document.querySelector('.btn').style;
 
-// Clear the input field
+// Clear the input field and reset main global variables
 function clear() {
 	input.value = "";
 	isOperator = false;
@@ -19,7 +19,7 @@ function clear() {
 // Add event listeners for all body elements
 // =========================================
 for(var i = 0; i < buttons.length; ++i) {
-	buttons[i].addEventListener( 'click', calculate, false );
+	buttons[i].addEventListener( 'click', calculate, false ); // each button gets an onclick event
 }
 
 document.querySelector('#input').onkeydown = function (e) { return false; } // no keyboard input allowed
@@ -40,31 +40,34 @@ function calculate() {
 		input.value += ( currentEl == "." && ( input.value.match(/\./g) || []).length > 0 )
 						? "" : currentEl;
 
-		if ( currentButton ) {
+		if ( currentButton ) { 		// Reset styles of locked buttons.
 			currentButton.style.borderColor = "";
 			currentButton.style.color = "";
 		}
 
-	} else if ( (/^\+\/-$/).test(currentEl) ) {
+	} else if ( (/^\+\/-$/).test(currentEl) ) { 	// if +/- button is pressed
 
 		if (input.value[0] != '-')
 			input.value = "-" + input.value;
 
-	} else if ( (/√/).test(currentEl) ) {
+	} else if ( (/√/).test(currentEl) ) { 	// if sqrt button is pressed
 
 		if ( input.value[0] != '-' )
 			input.value = Math.sqrt(parseFloat(input.value));
 
-	} else if ( (/[\+\-\*\/%\^]/).test(currentEl) ) {
-
+	} else if ( (/[\+\-\*\/%\^]/).test(currentEl) ) {	// if arithmetic operator button 
+														// is pressed (except sqrt)
 		if ( (input.value || "").length <= 0 ) { 
 			clear();
 			return false;
 		} else {
 
+			// Lock the style of the operation button to make user
+			// notice which operation they perform at the moment.
 			this.style.borderColor = "#1abc9c";
 			this.style.color = "#1abc84"
 			currentButton = this;
+
 
 			firstNum = (input.value || []).length > 0 ? input.value : firstNum;
 			input.value = "";
@@ -72,15 +75,15 @@ function calculate() {
 			isOperator = true;
 		}
 
-	} else if ( (/=/).test(currentEl) ) {
+	} else if ( (/=/).test(currentEl) ) {	// Equals button is pressed => calculate the result.
 
-		if ( currentButton ) {
+		if ( currentButton ) { 		// Reset styles of locked buttons.
 			currentButton.style.borderColor = "";
 			currentButton.style.color = "";
 		}
 		secondNum = input.value;
 
-		switch (currentOperator) {
+		switch (currentOperator) { // Commit the needed arithmetic operation.
 
 			case "*":
 				numResult = parseFloat(firstNum) * parseFloat(secondNum);
