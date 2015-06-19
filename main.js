c9ce203d90f6bbb84dc3543ee16585cf8967e0a8
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-06-20 00:33:16 +0300

    Added sqrt function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // Global variables
 // ================
 var input = document.getElementById( 'input' );
-input.value = "";
+input.value = ""; // The initial state if the input field
 var buttons = document.getElementsByClassName( 'btn' );
 var numResult, isOperator, currentOperator, firstNum, secondNum;
 
@@ -21,7 +21,7 @@ for(var i = 0; i < buttons.length; ++i) {
 	buttons[i].addEventListener( 'click', calculate, false );
 }
 
-document.querySelector('#input').onkeydown = function (e) { return false; }
+document.querySelector('#input').onkeydown = function (e) { return false; } // no keyboard input allowed
 
 // ==============================
 // Event handler for click events
@@ -41,8 +41,13 @@ function calculate() {
 
 	} else if ( (/^\+\/-$/).test(currentEl) ) {
 
-		if(input.value[0] != '-')
+		if (input.value[0] != '-')
 			input.value = "-" + input.value;
+
+	} else if ( (/√/).test(currentEl) ) {
+
+		if ( input.value[0] != '-' )
+			input.value = Math.sqrt(parseFloat(input.value));
 
 	} else if ( (/[\+\-\*\/%\^]/).test(currentEl) ) {
 
@@ -51,8 +56,8 @@ function calculate() {
 			return false;
 		};
 
-		firstNum = input.value;
-		input.value = "";
+		firstNum = (input.value || []).length > 0 ? input.value : firstNum;
+		input.value == "";
 		currentOperator = currentEl;
 		isOperator = true;
 
